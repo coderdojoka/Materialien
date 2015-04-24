788dcf8cc4ca95900a46ba11c0543ecaa4cb387b
me <mark@weinreuter.org>
2015-04-24 21:57:34 +0200

    Tex sources hinzugefügt. Beispiele Ordner für Pyhton Programme eingefügt.

---
+++ Python/Beispiele/eingabe.py
@@ -0,0 +1,9 @@
+# Liest einen Text von der Konsole ein ('\n erzeugt einen Zeilenumbruch)
+name = input("Wie heißt du?\n")	
+
+alter = input("Wie alt bist du?\n")
+
+# Gibt den eingelesenen Namen auf der Konsole aus
+print("Hallo", name)
+
+print("Du bist also" , alter , "Jahre alt!")
--- Python/py2cd/beispiele/beispiel_mathe.py
+++ Python/py2cd/beispiele/beispiel_mathe.py
@@ -10,13 +10,11 @@ sys.path.append("py2cd")
 
 from py2cd.spiel import Spiel
 from py2cd.farben import *
-from py2cd.flaeche import ZeichenFlaeche, neue_pygame_flaeche
 from py2cd.mathe import Plot
 
 
 Spiel.init(640, 480, "Hallo Mathe")
 zf = Spiel.gib_zeichen_flaeche()
-flaeche = ZeichenFlaeche(0, 0, neue_pygame_flaeche(300, 300, True), zf, (0, 0, 0, 0))
 
 # Plotet eine Sinusfunktion
 plot = Plot(lambda x: math.sin(x), -12, 12, zf, vergroesserung=20)
