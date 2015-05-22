647e6d9fb9a756813a9efdeb80d4edf888c1f9f2
me <mark@weinreuter.org>
2015-05-22 13:21:11 +0200

    Zahlenratencode verbessert

--- Python/Beispiele/zahlen_raten.py
+++ Python/Beispiele/zahlen_raten.py
@@ -3,8 +3,9 @@ import random
 zufallsZahl = random.randint(1,100)
 
 # Variable in der die geratente Zahl gespeichert wird
-# input(..) liest einen Text ein. Dieser Text wird mit int(...) in eine Zahl konvertiert
-gerateneZahl = int(input("Rate die Zahl: "))
+# input(..) liest einen Text ein. Dieser Text wird mit int(..) in eine Zahl konvertiert
+print("Willkommen beim Zahlenraten.")
+gerateneZahl = int(input("Bitte gib deinen ersten Tipp ab: "))
 
 # Variable in der die Anzahl der Versuche gespeichert werden
 versuche = 1
@@ -14,10 +15,10 @@ while gerateneZahl != zufallsZahl:
 
     # Hinweis, wie groß die Zahl ist    
     if gerateneZahl < zufallsZahl:
-        print("Zu klein")
+        print("Zu klein!")
 
     else:
-        print("Zu groß")
+        print("Zu groß!")
 
     # Neuer Versuch
     gerateneZahl = int(input("Rate die Zahl: "))
@@ -26,6 +27,6 @@ while gerateneZahl != zufallsZahl:
     versuche += 1
 
     
-# Anzahl der Versuche ausgeben
-print("Du hast", versuche, " Versuche gebraucht")
+# Anzahl der Versuche ausgeben (außerhalb der while-Schleife)
+print("Du hast", versuche, " Versuche gebraucht.")
 
