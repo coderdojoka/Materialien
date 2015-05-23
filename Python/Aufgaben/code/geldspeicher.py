6ed22f02854257ad08452e61865b2e33d9be5b64
me <mark@weinreuter.org>
2015-05-23 09:59:00 +0200

    Geldspeicheraufgabe hinzugefügt

---
+++ Python/Aufgaben/code/geldspeicher.py
@@ -0,0 +1,47 @@
+__author__ = 'Mark Weinreuter'
+
+import random
+
+# versteckte Kombination :D
+__kombi_gs_v1 = [
+    random.randint(0, 9),
+    random.randint(0, 9),
+    random.randint(0, 9)
+]
+
+# versteckte Kombination (zufällige ascii a-z)
+__kombi_gs_v2 = [
+    chr(random.randint(97, 122)),
+    chr(random.randint(97, 122)),
+    chr(random.randint(97, 122))
+]
+
+
+def rate_passwort(z1, z2, z3):
+    """
+    Testet die Kombination des Geldspeichers. In dieser Version besteht die Kombination aus Zahlen.
+    :param z1: Die erste Ziffer
+    :type z1: int
+    :param z2: Die zweite Ziffer
+    :type z2: int
+    :param z3: Die dritte Ziffer
+    :type z3: int
+    """
+
+    return __test_kombination(z1, z2, z3, __kombi_gs_v1)
+
+
+def rate_passwort_buchstaben(c1, c2, c3):
+    __test_kombination(c1, c2, c3, __kombi_gs_v2)
+
+
+def __test_kombination(z1, z2, z3, __kombi):
+
+    # alle drei ziffern vergleichen
+    if z1 == __kombi[0] and z2 == __kombi[2] and z3 == __kombi[3]:
+
+        # Kombination ist richtig
+        print("Gelspeicher geöffnet")
+        return True
+
+    return False
---
+++ Python/Aufgaben/code/geldspeicher_test.py
@@ -0,0 +1,8 @@
+from geldspeicher import rate_passwort
+
+# Versuch das Passwort zu raten: 123 -> False/True
+treffer = rate_passwort(1, 2, 3)
+print(treffer) # Gibt True oder False aus
+
+# Hier könnte deine Lösung stehen
+
