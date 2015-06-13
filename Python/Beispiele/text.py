b20740cd7abfb819c144148d4b34303426b0a95c
me <mark@weinreuter.org>
2015-06-13 16:44:34 +0200

    Tutorial für Text

---
+++ Python/Beispiele/text.py
@@ -0,0 +1,24 @@
+# eine Variable mit dem Wert 'Hallo Welt'
+meinText = "Hallo Welt"
+print(meinText)
+
+name = "Mark"
+meinText = "Hallo " + name  # Textstücke zusammenkleben
+print(meinText)
+
+# Kombinierte Ausgabe
+print("Hallo", name)
+
+meinText = "Hallo" * 2
+print(meinText)  # gibt 'HalloHallo' aus
+
+laenge = len(meinText)  # Länge des Textes ermitteln
+print(laenge)
+
+meinText = 'Hallo'
+buchstabe = meinText[2]  # dritter Buchstabe! Die erste Stelle ist 0!
+print(buchstabe)  # Gibt 'l' aus
+
+# Dies erzeugt einen Fehler! Text sind nicht veränderlich
+meinText[3] = 'b'
+
