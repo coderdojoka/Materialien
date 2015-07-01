3d500eec90f719721a8a5d6d54cea0a0b72d7352
me <mark@weinreuter.org>
2015-07-01 16:29:25 +0200

    Umbenannt

--- Python/usb_stick_update.py
+++ Python/usb_stick_update.py
@@ -10,19 +10,34 @@ from zipfile import ZipFile
 
 # Dieses Skript sollte in dem Ordner liegen, indem auch das Materialien Verzeichnis liegt
 
+def loesche_unnoetige_dateien(base):
+
+    for root, dirs, files in os.walk(base):
+        for file in files:
+            if any(file.lower().endswith(ext) for ext in [".tex", ".cls", ".sty"]):
+                print("Removing: ", file)
+                os.remove(os.path.join(root, file))
+
 def git_pull():
 
     # Falls bereits vorhanden => pull
-    if os.path.isdir("Materialien"):
+    if os.path.isdir("Materialien/.git"):
         os.chdir("Materialien")
         try:
             subprocess.call(["git", "pull"])
+            os.chdir("..")
+            return True
         except OSError as e:
-            os.chdir("..") # Wir müssen wieder eins hoch :D
+            os.chdir("..")
             return False
 
     # Ansonsten klonen wir das Repo neu
     else:
+
+        # ggf. alten Ordner löschen
+        if os.path.isdir("Materialien"):
+            shutil.rmtree("Materialien")
+
         try:
             subprocess.call(["git", "clone", "https://github.com/coderdojoka/Materialien"])
         except OSError as e:
@@ -30,11 +45,7 @@ def git_pull():
 
     return True
 
-# Änderungen laden
-if not git_pull():
-    print("Git ist nicth installiert => Lade zip-Datei.")
-    print("Dies kann eine Weile dauern...")
-
+def lade_zip():
     # zip datei herrunterladen un direkt entpacken (in memory)
     url = urlopen("https://github.com/coderdojoka/Materialien/archive/master.zip")
 
@@ -51,8 +62,14 @@ if not git_pull():
     print("Fertig!")
 
 
-# Dateien entfernen
+# Änderungen laden
+if not git_pull():
+    print("Git ist nicht installiert => Lade zip-Datei.")
+    print("Dies kann eine Weile dauern...")
+    lade_zip()
 
+# Lösche unnötige Dateien, z.B. tex files
+loesche_unnoetige_dateien("Materialien")
 
 # Update-Skript ersetzen
-shutil.move("Materialien/Python/coderdojo_update.py", "coderdojo_update.py")
+shutil.copy("Materialien/Python/usb_stick_update.py", "usb_stick_update.py")
